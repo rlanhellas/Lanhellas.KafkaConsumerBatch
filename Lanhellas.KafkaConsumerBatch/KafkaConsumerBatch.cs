dddddd57934967119f90610001d49607b14521c4
Ronaldo Lanhellas <ronaldo.lanhellas@gmail.com>
2021-05-14 14:20:56 -0300

    Uploading project README and source

---
+++ Lanhellas.KafkaConsumerBatch/KafkaConsumerBatch.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+
+namespace Lanhellas.KafkaConsumerBatch
+{
+    public class KafkaConsumerBatch<TKey,TValue>
+    {
+
+        private readonly ICollection<ConsumeResult<TKey, TValue>> _records;
+        private readonly int _batchSize;
+        private readonly TimeSpan _maxWaitTime;
+        private readonly IConsumer<TKey, TValue> _consumer;
+        private readonly ILogger _logger;
+
+        public KafkaConsumerBatch(IConsumer<TKey,TValue> consumer, int batchSize, TimeSpan maxWaitTime, ILogger logger)
+        {
+            _records = new List<ConsumeResult<TKey, TValue>>();
+            _batchSize = batchSize;
+            _maxWaitTime = maxWaitTime;
+            _consumer = consumer;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Consume records in batch, return to caller when batchSize is reached or when maxWaitTime is reached
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<ConsumeResult<TKey,TValue>> ConsumeBatch()
+        {
+              _records.Clear();
+              StartConsume();
+              return _records;
+        }
+
+        /// <summary>
+        /// Seek offets to minimum in batch, of all partitions that was consumed in this batch flow
+        /// </summary>
+        /// <param name="consumer"></param>
+        public void SeekBatch()
+        {
+            var sublists = _records.GroupBy(r => r.Partition).ToList();
+
+            foreach (var sublist in sublists)
+            {
+                var minOffset = sublist.Min(s => s.Offset.Value);
+                var partition = sublist.Key.Value;
+                _logger.LogDebug("Seek Partition {} to Offset {}", partition, minOffset);
+                _consumer.Seek(new TopicPartitionOffset(sublist.First().Topic, new Partition(partition), new Offset(minOffset) ));
+            }
+        }
+
+        private void StartConsume()
+        {
+            while (true)
+            {
+                _logger.LogDebug("Actual Records Size {}, BatchSize {}", _records.Count, _batchSize);
+                if (_records.Count >= _batchSize)
+                {
+                    break;
+                }
+
+                var result = _consumer.Consume(_maxWaitTime);
+                if (result != null)
+                {
+                    _records.Add(result);
+                }
+                else
+                {
+                    _logger.LogDebug("Reached MaxWaitTime {}, Returning batch with {} results to caller", _maxWaitTime, _records.Count);
+                    break;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Lanhellas.KafkaConsumerBatch
-Wrapper that helps Confuent.Kafka library to work like a Batch Consumer
+# Introduction
+This library born from a needed to consume messages in batch style, using the famous Confluent.Kafka library (https://www.nuget.org/packages/Confluent.Kafka). It works as a Wrapper providing the capability to return a List of results instead of a single result *(see a real example in the 'How to use ?' section).*
+
+
+## How to use?
+
+You first need to create a consumer from *Confluent.Kafka* and **subscribe** in a topic, see:
+
+    var config = new ConsumerConfig  
+    {  
+      BootstrapServers = "localhost:9092",  
+      GroupId = "mygroup"
+    };  
+    var consumer = new ConsumerBuilder<string,string>(config).Build();  
+    consumer.Subscribe(topic);
+
+Until here, nothing new, right? :D 
+
+Now you will need to create the **Batch Wrapper,** using the Builder Pattern, see how:
+
+    var consumerBatch = KafkaConsumerBatchBuilder<string, string>.Config()  
+     .WithConsumer(consumer)  
+     .WithLogger(_logger)  
+     .WithBatchSize(50)  
+     .WithMaxWaitTime(TimeSpan.FromSeconds(10))  
+     .Build();
+
+Take note of some important properties:
+
+ 1. `WithConsumer(IConsumer<TKey, TValue> consumer)`: You must pass the consumer object that you created before with `new ConsumerBuilder<,>()`
+ 2. `WithLogger(ILogger logger)`: You must pass an instance of ILogger, in this way you can trace any problems.
+ 3. `WithBatchSize(int batchSize)`: How many records do you want to ? Inform here the quantity.
+ 4. `WithMaxWaitTime(TimeSpan maxWaitTime)`: Inform here how many times do you want to wait before returning the consumer. If *batchSize* is not reached before *maxWaitTime* the batch will return immediately.
+
+And finally, you can consume in batch:
+
+    var results = consumerBatch.ConsumeBatch();
+
+Now you can seek (redelivery all messages in batch) in batch too:
+
+    consumerBatch.SeekBatch();
+And if you need to commit, just do it, the same way as you usually do (**You don't need to use consumerBatch here**):
+
+    consumer.Commit();
+
+
+
+## You can contribute
+
+Feel free to contribute with your suggestions and improvements, just make a Pull Request and be happy. 
